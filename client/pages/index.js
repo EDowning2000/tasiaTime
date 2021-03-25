42921fd43a16f9afb5755078ddd6db9bfa7936e0
Emerson Downing <emersondowning@gmail.com>
2021-03-25 11:50:11 -0400

    got the first portion of the scorecard finishe

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,22 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.mainContainer}>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
+      <div className={styles.lightGreen}></div>
+      <div className={styles.darkGreen}></div>
       <div className={styles.secondaryContainer}></div>
     </div>
   );
