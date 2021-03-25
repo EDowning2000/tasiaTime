fbff6f5ea39bdb2d0fe50d884d037c1b900283ab
Emerson Downing <emersondowning@gmail.com>
2021-03-25 12:52:40 -0400

    getting the front of the card finished, just need to add the image

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,23 +2,29 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.lightGreen}></div>
-      <div className={styles.darkGreen}></div>
-      <div className={styles.secondaryContainer}></div>
+      <div className={styles.firstContainer}>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.main}>
+          <div className={styles.main2}>
+            <div className={styles.main3}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
