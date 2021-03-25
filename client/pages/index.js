ace7a024c158a90748761172800a45593a623162
Emerson Downing <emersondowning@gmail.com>
2021-03-25 10:54:18 -0400

    got the card centered

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,8 @@
 import styles from "../styles/Home.module.css";
 export default function Home() {
-  return <div className={styles.mainContainer}></div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.secondaryContainer}></div>
+    </div>
+  );
 }
