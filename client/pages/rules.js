74c4d6c5a7570fad96e0754c40cfd375efd83665
Emerson Downing <emersondowning@gmail.com>
2021-04-06 14:13:55 -0400

    rules headers are finished and looking nice. just need sub content for them

--- client/pages/rules.js
+++ client/pages/rules.js
@@ -5,6 +5,12 @@ export default function Rules() {
     <div className={styles.mainContainer}>
       <div className={styles.firstContainer}>
         <h1 className={styles.rulesOfPlay}>RULES OF PLAY</h1>
+        <div className={styles.individualRulesContainer}>
+          <h4 className={styles.individualRule}>TEE OFF</h4>
+          <h4 className={styles.individualRule}>MAXIMUM GROUP SIZE</h4>
+          <h4 className={styles.individualRule}>OUT-OF-BOUNDS</h4>
+          <h4 className={styles.individualRule}>SAFETY</h4>
+        </div>
       </div>
     </div>
   );
