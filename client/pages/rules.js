936917ffde8561ec6c8ee7998323e7ea12a0853a
Emerson Downing <emersondowning@gmail.com>
2021-04-06 13:45:07 -0400

    got my card centered with flex correctly

--- client/pages/rules.js
+++ client/pages/rules.js
@@ -3,28 +3,7 @@ import styles from "../styles/Rules.module.css";
 export default function Rules() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.firstContainer}>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.lightGreen}></div>
-        <div className={styles.darkGreen}></div>
-        <div className={styles.center}>
-          <div className={styles.center2}></div>
-          <></>
-        </div>
-      </div>
+      <div className={styles.firstContainer}></div>
     </div>
   );
 }
