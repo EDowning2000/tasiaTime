097bec48d8a6bc8dea0f68b7951e841bc8b51006
Emerson Downing <emersondowning@gmail.com>
2021-04-06 14:40:59 -0400

    got the rules looking perfect, just looking for some little golf ball icons now

--- client/pages/rules.js
+++ client/pages/rules.js
@@ -6,10 +6,30 @@ export default function Rules() {
       <div className={styles.firstContainer}>
         <h1 className={styles.rulesOfPlay}>RULES OF PLAY</h1>
         <div className={styles.individualRulesContainer}>
-          <h4 className={styles.individualRule}>TEE OFF</h4>
-          <h4 className={styles.individualRule}>MAXIMUM GROUP SIZE</h4>
-          <h4 className={styles.individualRule}>OUT-OF-BOUNDS</h4>
-          <h4 className={styles.individualRule}>SAFETY</h4>
+          <h5 className={styles.individualRule}>TEE OFF</h5>
+          <h6 className={styles.individualRuleInfo}>
+            ALL PLAYERS IN A GROUP HIT THEIR FIRST SHOTS, THEN PROCEED TOGETHER
+            TO THEIR SECOND SHOT. SIX (6) STROKE LIMIT ON ALL HOLES.
+          </h6>
+          <h5 className={styles.individualRule}>MAXIMUM GROUP SIZE</h5>
+          <h6 className={styles.individualRuleInfo}>
+            NO MORE THAN FIVE (5) PLAYERS TO A GROUP. PLEASE ALLOW FASTER GROUPS
+            TO ADVANCE.
+          </h6>
+          <h5 className={styles.individualRule}>OUT-OF-BOUNDS</h5>
+          <h6 className={styles.individualRuleInfo}>
+            IF YOUR BALL LANDS IN ANY LANDSCAPING, NATURAL GRASS, OR WATER
+            HAZARD, IT IS CONSIDERED OUT-OF-BOUNDS. PLEASE PLACE THE BALL BACK
+            ONTO THE COURSE. ONCE CLUB HEAD AWAY FROM THE AREA AND TAKE ONE
+            PENALTY STROKE.
+          </h6>
+          <h5 className={styles.individualRule}>SAFETY</h5>
+          <h6 className={styles.individualRuleInfo}>
+            PLEASE AVOID TAKING FULL SWINGS WITH THE PUTTERS AND BE AWARE OF
+            YOUR SURROUNDINGS AT ALL TIMES. KEEP A SAFE DISTANCE BEHIND ANYONE
+            PUTTING. FOR SAFETY, PLEASE STAY ON THE COURSE AND WALKWAYS. REFRAIN
+            FROM CLIMBING ON THE SHOW PROPS.
+          </h6>
         </div>
       </div>
     </div>
