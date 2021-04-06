2aa2c0255532fb9f128ae9ea68417b9734b94aa7
Emerson Downing <emersondowning@gmail.com>
2021-04-06 13:23:37 -0400

    got the center of the rules page working nicely

--- client/pages/rules.js
+++ client/pages/rules.js
@@ -2,8 +2,29 @@ import styles from "../styles/Rules.module.css";
 
 export default function Rules() {
   return (
-    <>
-      <div> Hello world</div>
-    </>
+    <div className={styles.mainContainer}>
+      <div className={styles.firstContainer}>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.lightGreen}></div>
+        <div className={styles.darkGreen}></div>
+        <div className={styles.center}>
+          <div className={styles.center2}></div>
+          <></>
+        </div>
+      </div>
+    </div>
   );
 }
