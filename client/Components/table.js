0f441d3803f8839851cd1dbe14f6ad7e51ffafcf
Emerson Downing <emersondowning@gmail.com>
2021-04-06 19:53:56 -0400

    getting the tables for the scorecard all wrapped up

---
+++ client/Components/table.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Table.module.css";
+
+export default function Table() {
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+}
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import Table from "../Components/table";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Fantasia Gardens Mini Golf</title>
+        <meta charSet="utf-8" />
+      </Head>
+      {/* <Component {...pageProps} /> */}
+      <Table />
+    </>
+  );
 }
 
 export default MyApp;
