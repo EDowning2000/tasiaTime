b563079b69b4e41b2a908f976a84a5a7b12c7a5c
Emerson Downing <emersondowning@gmail.com>
2021-04-06 14:04:23 -0400

    downloading fonts from the web to make them perfect

--- client/pages/rules.js
+++ client/pages/rules.js
@@ -3,7 +3,9 @@ import styles from "../styles/Rules.module.css";
 export default function Rules() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.firstContainer}></div>
+      <div className={styles.firstContainer}>
+        <h3 className={styles.rulesOfPlay}>RULES OF PLAY</h3>
+      </div>
     </div>
   );
 }
