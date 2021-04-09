cd362a69e8cd8d9cf0ee73132a486e01da71a067
Emerson Downing <emersondowning@gmail.com>
2021-04-09 11:01:02 -0400

    finished the ui of this stupid table finally

--- client/Components/table.js
+++ client/Components/table.js
@@ -30,6 +30,169 @@ export default function Table() {
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
         </tr>
+        <tr>
+          <th className={styles.holeNumber}>2</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>3</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>4</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>5</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>6</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>7</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>8</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>9</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>10</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>11</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>12</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>13</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>14</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>15</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>16</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>17</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+        <tr>
+          <th className={styles.holeNumber}>18</th>
+          <th className={styles.holePar}>4</th>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+          <td className={styles.dataThree}>a</td>
+        </tr>
+
+        <tr>
+          <th className={styles.total}>Total</th>
+          <th className={styles.totalPar}>72</th>
+          <td className={styles.dataFour}>a</td>
+          <td className={styles.dataFour}>a</td>
+          <td className={styles.dataFour}>a</td>
+          <td className={styles.dataFour}>a</td>
+          <td className={styles.dataFour}>a</td>
+        </tr>
       </table>
     </>
   );
