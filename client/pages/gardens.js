889487c2c78653b8447388717cc6e0ebfd7aa1ad
Emerson Downing <emersondowning@gmail.com>
2021-04-10 10:55:55 -0400

    got the start of the gardens page working

--- client/pages/gardens.js
+++ client/pages/gardens.js
@@ -1,9 +1,24 @@
 import styles from "../styles/Gardens.module.css";
+import golfBall from "../Images/golfBall.png";
 
 export default function Gardens() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.firstContainer}></div>
+      <div className={styles.firstContainer}>
+        <h1 className={styles.gardensHeader}>THE GARDENS COURSE</h1>
+        <h4 className={styles.gardensSubHeader}>The Hippo-est Golf in Town!</h4>
+        <div className={styles.mainPictureContainer}>
+          <div className={styles.pictureContainer}>
+            <img src={golfBall} className={styles.golfBallPicture} />
+          </div>
+          <div className={styles.pictureContainer}>
+            <img src={golfBall} className={styles.golfBallPicture} />
+          </div>
+          <div className={styles.pictureContainer}>
+            <img src={golfBall} className={styles.golfBallPicture} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
