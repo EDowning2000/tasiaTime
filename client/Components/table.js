88881e70de9724f4e3b6a6050e0f11920128e5da
Emerson Downing <emersondowning@gmail.com>
2021-04-13 16:47:23 -0400

    constants are in, and props have been added to the table

--- client/Components/table.js
+++ client/Components/table.js
@@ -1,6 +1,26 @@
 import styles from "../styles/Table.module.css";
 
-export default function Table() {
+export default function Table({
+  HOLE1,
+  HOLE2,
+  HOLE3,
+  HOLE4,
+  HOLE5,
+  HOLE6,
+  HOLE7,
+  HOLE8,
+  HOLE9,
+  HOLE10,
+  HOLE11,
+  HOLE12,
+  HOLE13,
+  HOLE14,
+  HOLE15,
+  HOLE16,
+  HOLE17,
+  HOLE18,
+  TOTAL,
+}) {
   return (
     <>
       <table className={styles.mainTable}>
@@ -23,7 +43,7 @@ export default function Table() {
       <table className={styles.secondaryTable}>
         <tr>
           <th className={styles.holeNumber}>1</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE1}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -32,7 +52,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>2</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE2}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -41,7 +61,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>3</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE3}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -50,7 +70,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>4</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE4}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -59,7 +79,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>5</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE5}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -68,7 +88,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>6</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE6}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -77,7 +97,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>7</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE7}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -86,7 +106,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>8</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE8}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -95,7 +115,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>9</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE9}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -104,7 +124,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>10</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE10}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -113,7 +133,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>11</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE11}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -122,7 +142,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>12</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE12}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -131,7 +151,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>13</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE13}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -140,7 +160,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>14</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE14}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -149,7 +169,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>15</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE15}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -158,7 +178,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>16</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE16}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -167,7 +187,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>17</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE17}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -176,7 +196,7 @@ export default function Table() {
         </tr>
         <tr>
           <th className={styles.holeNumber}>18</th>
-          <th className={styles.holePar}>4</th>
+          <th className={styles.holePar}>{HOLE18}</th>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
           <td className={styles.dataThree}>a</td>
@@ -186,7 +206,7 @@ export default function Table() {
 
         <tr>
           <th className={styles.total}>Total</th>
-          <th className={styles.totalPar}>72</th>
+          <th className={styles.totalPar}>{TOTAL}</th>
           <td className={styles.dataFour}>a</td>
           <td className={styles.dataFour}>a</td>
           <td className={styles.dataFour}>a</td>
--- client/pages/gardens.js
+++ client/pages/gardens.js
@@ -20,7 +20,27 @@ export default function Gardens() {
           </div>
         </div>
         <div className={styles.secondContainer}>
-          <Table />
+          <Table
+            HOLE1={}
+            HOLE2={}
+            HOLE3={}
+            HOLE4={}
+            HOLE5={}
+            HOLE6={}
+            HOLE7={}
+            HOLE8={}
+            HOLE9={}
+            HOLE10={}
+            HOLE11={}
+            HOLE12={}
+            HOLE13={}
+            HOLE14={}
+            HOLE15={}
+            HOLE16={}
+            HOLE17={}
+            HOLE18={}
+            TOTAL={}
+          />
         </div>
       </div>
     </div>
