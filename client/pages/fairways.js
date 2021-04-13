f92dde234a01b00e9ae1cf59fa7db5a87f55a789
Emerson Downing <emersondowning@gmail.com>
2021-04-13 17:05:13 -0400

    all constants are added into the props

--- client/pages/fairways.js
+++ client/pages/fairways.js
@@ -22,27 +22,28 @@ export default function Fairways() {
             <img src={golfBall} className={styles.golfBallPicture} />
           </div>
         </div>
+        {console.log(HOLES[19])}
         <div className={styles.secondContainer}>
           <Table
-            HOLE1={}
-            HOLE2={}
-            HOLE3={}
-            HOLE4={}
-            HOLE5={}
-            HOLE6={}
-            HOLE7={}
-            HOLE8={}
-            HOLE9={}
-            HOLE10={}
-            HOLE11={}
-            HOLE12={}
-            HOLE13={}
-            HOLE14={}
-            HOLE15={}
-            HOLE16={}
-            HOLE17={}
-            HOLE18={}
-            TOTAL={}
+            HOLE1={HOLES[19].FAIRWAYS_1}
+            HOLE2={HOLES[20].FAIRWAYS_2}
+            HOLE3={HOLES[21].FAIRWAYS_3}
+            HOLE4={HOLES[22].FAIRWAYS_4}
+            HOLE5={HOLES[23].FAIRWAYS_5}
+            HOLE6={HOLES[24].FAIRWAYS_6}
+            HOLE7={HOLES[25].FAIRWAYS_7}
+            HOLE8={HOLES[26].FAIRWAYS_8}
+            HOLE9={HOLES[27].FAIRWAYS_9}
+            HOLE10={HOLES[28].FAIRWAYS_10}
+            HOLE11={HOLES[29].FAIRWAYS_11}
+            HOLE12={HOLES[30].FAIRWAYS_12}
+            HOLE13={HOLES[31].FAIRWAYS_13}
+            HOLE14={HOLES[32].FAIRWAYS_14}
+            HOLE15={HOLES[33].FAIRWAYS_15}
+            HOLE16={HOLES[34].FAIRWAYS_16}
+            HOLE17={HOLES[35].FAIRWAYS_17}
+            HOLE18={HOLES[36].FAIRWAYS_18}
+            TOTAL={HOLES[37].FAIRWAYS_TOTAL}
           />
         </div>
       </div>
