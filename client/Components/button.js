9adaf9297aa3a391d5fcd74fd79ac4b0f1c2a431
Emerson Downing <emersondowning@gmail.com>
2021-04-14 14:02:24 -0400

    working on the props for the routing

--- client/Components/button.js
+++ client/Components/button.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import Link from "next/link";
 import styles from "../styles/Button.module.css";
 import arrow from "../Images/routerArrow.png";
-export default function Button() {
+
+export default function Button({
+  text,
+  routePrevious,
+  routeNext,
+  nextHidden,
+  previousHidden,
+}) {
+  const routeBack = useState("");
+  const routeForward = useState("");
+  const back = useState("");
+  const forward = useState("");
   return (
     <>
       <div className={styles.mainContainer}>
         {" "}
-        <img src={arrow} alt="navigate forward" className={styles.arrowBack} />
+        <Link href={routePrevious}>
+          <img
+            src={arrow}
+            alt="navigate forward"
+            className={styles.arrowBack}
+          />
+        </Link>
         <div className={styles.secondaryContainer}>
           <div className={styles.stripeContainer}>
+            <h3>{text}</h3>
             <div className={styles.lightGreenStripeFirst}></div>
             <div className={styles.darkGreenStripe}></div>
             <div className={styles.lightGreenStripe}></div>
@@ -15,11 +35,13 @@ export default function Button() {
             <div className={styles.lightGreenStripeLast}></div>
           </div>
         </div>
-        <img
-          src={arrow}
-          alt="navigate forward"
-          className={styles.arrowForward}
-        />
+        <Link href={routeNext}>
+          <img
+            src={arrow}
+            alt="navigate forward"
+            className={styles.arrowForward}
+          />
+        </Link>
       </div>
     </>
   );
