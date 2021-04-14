c4a8dac21ef2af8155b91fbadab3afb5f8ae4f4b
Emerson Downing <emersondowning@gmail.com>
2021-04-14 14:44:12 -0400

    got all the props into place along with the logic for the button component now just need to implement

--- client/Components/button.js
+++ client/Components/button.js
@@ -7,40 +7,66 @@ export default function Button({
   text,
   routePrevious,
   routeNext,
-  nextHidden,
   previousHidden,
 }) {
-  let forwardHidden = { nextHidden };
   let backHiden = { previousHidden };
-  return (
-    <>
-      <div className={styles.mainContainer}>
-        {" "}
-        <Link href="/">
-          <img
-            src={arrow}
-            alt="navigate forward"
-            className={styles.arrowBack}
-          />
-        </Link>
-        <div className={styles.secondaryContainer}>
-          <div className={styles.stripeContainer}>
-            <h3>{text}</h3>
-            <div className={styles.lightGreenStripeFirst}></div>
-            <div className={styles.darkGreenStripe}></div>
-            <div className={styles.lightGreenStripe}></div>
-            <div className={styles.darkGreenStripe}></div>
-            <div className={styles.lightGreenStripeLast}></div>
+
+  if (backHidden) {
+    return (
+      <>
+        <div className={styles.mainContainer}>
+          {" "}
+          <div className={styles.secondaryContainer}>
+            <div className={styles.stripeContainer}>
+              <h3>{text}</h3>
+              <div className={styles.lightGreenStripeFirst}></div>
+              <div className={styles.darkGreenStripe}></div>
+              <div className={styles.lightGreenStripe}></div>
+              <div className={styles.darkGreenStripe}></div>
+              <div className={styles.lightGreenStripeLast}></div>
+            </div>
           </div>
+          <Link href={routeNext}>
+            <img
+              src={arrow}
+              alt="navigate forward"
+              className={styles.arrowForward}
+            />
+          </Link>
         </div>
-        <Link href="/">
-          <img
-            src={arrow}
-            alt="navigate forward"
-            className={styles.arrowForward}
-          />
-        </Link>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.mainContainer}>
+          {" "}
+          <Link href={routePrevious}>
+            <img
+              src={arrow}
+              alt="navigate forward"
+              className={styles.arrowBack}
+            />
+          </Link>
+          <div className={styles.secondaryContainer}>
+            <div className={styles.stripeContainer}>
+              <h3>{text}</h3>
+              <div className={styles.lightGreenStripeFirst}></div>
+              <div className={styles.darkGreenStripe}></div>
+              <div className={styles.lightGreenStripe}></div>
+              <div className={styles.darkGreenStripe}></div>
+              <div className={styles.lightGreenStripeLast}></div>
+            </div>
+          </div>
+          <Link href={routeNext}>
+            <img
+              src={arrow}
+              alt="navigate forward"
+              className={styles.arrowForward}
+            />
+          </Link>
+        </div>
+      </>
+    );
+  }
 }
