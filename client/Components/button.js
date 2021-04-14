5e41d9f551fa1890e993c3f8a85aae40b3f8da7d
Emerson Downing <emersondowning@gmail.com>
2021-04-14 13:26:49 -0400

    working on the arrow styles and trying to optimize all the photos

--- client/Components/button.js
+++ client/Components/button.js
@@ -1,16 +1,26 @@
 import styles from "../styles/Button.module.css";
+import arrow from "../Images/arrow.png";
 export default function Button() {
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.secondaryContainer}>
-        <div className={styles.stripeContainer}>
-          <div className={styles.lightGreenStripeFirst}></div>
-          <div className={styles.darkGreenStripe}></div>
-          <div className={styles.lightGreenStripe}></div>
-          <div className={styles.darkGreenStripe}></div>
-          <div className={styles.lightGreenStripeLast}></div>
+    <>
+      <div className={styles.mainContainer}>
+        {" "}
+        <img src={arrow} alt="navigate forward" className={styles.arrowBack} />
+        <div className={styles.secondaryContainer}>
+          <div className={styles.stripeContainer}>
+            <div className={styles.lightGreenStripeFirst}></div>
+            <div className={styles.darkGreenStripe}></div>
+            <div className={styles.lightGreenStripe}></div>
+            <div className={styles.darkGreenStripe}></div>
+            <div className={styles.lightGreenStripeLast}></div>
+          </div>
         </div>
+        <img
+          src={arrow}
+          alt="navigate forward"
+          className={styles.arrowForward}
+        />
       </div>
-    </div>
+    </>
   );
 }
