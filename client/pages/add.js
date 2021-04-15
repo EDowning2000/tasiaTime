915628ae4b3aaeb90f9b09e87b66ec4de018f255
Emerson Downing <emersondowning@gmail.com>
2021-04-15 18:43:16 -0400

    the stupid dropdowns are proving to be more difficult than i thought they would be

--- client/pages/add.js
+++ client/pages/add.js
@@ -1,12 +1,32 @@
+import React from "react";
 import styles from "../styles/Add.module.css";
-import Button from "../Components/button";
-import { Dropdown } from "semantic-ui-react";
+import ButtonNav from "../Components/button";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
 export default function Add() {
   const courseOptions = [
     { key: "The Fairways", text: "The Fairways", value: "The Fairways" },
     { key: "The Gardens", text: "The Gardens", value: "The Gardens" },
   ];
-  const playerOptions = [{}, {}, {}, {}, {}];
+  const playerOptions = [
+    { key: "One", text: "One", value: "1" },
+    { key: "Two", text: "Two", value: "2" },
+    { key: "Three", text: "Three", value: "3" },
+    { key: "Four", text: "Four", value: "4" },
+    { key: "Five", text: "Five", value: "5" },
+  ];
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div className={styles.mainContainer}>
@@ -33,13 +53,30 @@ export default function Add() {
                 WHICH COURSE ARE YOU PLAYING
               </h3>
 
+              <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                Choose A Course
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>The Fairways</MenuItem>
+                <MenuItem onClick={handleClose}>The Gardens</MenuItem>
+              </Menu>
               <h3 className={styles.playerCountHeader}>HOW MANY PLAYERS</h3>
             </div>
           </div>
         </div>
       </div>
       <div className={styles.buttonContainer}>
-        <Button
+        <ButtonNav
           routePrevious="/"
           routeNext="/rules"
           text="Course Descriptions"
