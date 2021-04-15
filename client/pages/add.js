35004bffc3d8e326db2df0f1f10630cd400a3078
Emerson Downing <emersondowning@gmail.com>
2021-04-15 17:46:32 -0400

    getting semantic ui to bring in the dropdown component

--- client/pages/add.js
+++ client/pages/add.js
@@ -1,7 +1,12 @@
 import styles from "../styles/Add.module.css";
 import Button from "../Components/button";
-
+import { Dropdown } from "semantic-ui-react";
 export default function Add() {
+  const courseOptions = [
+    { key: "The Fairways", text: "The Fairways", value: "The Fairways" },
+    { key: "The Gardens", text: "The Gardens", value: "The Gardens" },
+  ];
+  const playerOptions = [{}, {}, {}, {}, {}];
   return (
     <>
       <div className={styles.mainContainer}>
@@ -27,6 +32,7 @@ export default function Add() {
               <h3 className={styles.coursePickerHeader}>
                 WHICH COURSE ARE YOU PLAYING
               </h3>
+
               <h3 className={styles.playerCountHeader}>HOW MANY PLAYERS</h3>
             </div>
           </div>
