a80f67cf09f234a986579f278d11a612c92033e3
Emerson Downing <emersondowning@gmail.com>
2021-04-15 13:48:40 -0400

    courses page is finishe

--- client/pages/courses.js
+++ client/pages/courses.js
@@ -1,5 +1,6 @@
 import styles from "../styles/Courses.module.css";
 import Button from "../Components/button";
+import golfBall from "../Images/golfBall.png";
 export default function Courses() {
   return (
     <>
@@ -23,29 +24,36 @@ export default function Courses() {
           <div className={styles.darkGreen}></div>
           <div className={styles.center}>
             <div className={styles.center2}>
-              <h3 className={styles.gardensHeader}>THE GARDENS COURSE</h3>
-              <div className={styles.courseInfoContainer}>
-                <img />
-                <h6 className={styles.gardensInfo}>
-                  TURN MINIATURE GOLFING INTO A SYMPHONY OF FUN FOR THE WHOLE
-                  FAMILY. THE GARDENS COURSE IS 18 HOLES OF WHIMSICAL CHALLENGES
-                  THAT TAKE PLAYERS THROUGH FIVE ORCHESTRAL MOMENTS OF THE
-                  DISNEY CLASSIC, FANTASIA. DANCING FOUNTAINS, MUSICAL
-                  INSTRUMENTS AND ANIMATED SATUES ADD TO A GOLFING FANTASY
-                  YOU'LL ALWAYS REMEMBER.
-                </h6>
-              </div>
-              <h3 className={styles.fairwaysHeader}>THE FAIRWAYS COURSE</h3>
-              <div className={styles.courseInfoContainer}>
-                <img />
-                <h6 className={styles.fairwaysInfo}>
-                  FOR A UNIQUEGOLFING EXPERIENCE, THIS COURSE BRINGS EXCITING
-                  CHALLENGE. ENJOY PURE GOLF ON A SMALL SCALE, COMPLETE WITH
-                  WATER HAZARDS, SAND TRAPS, GRASSY ROUGHS AND DOG-LEG BENDS
-                  THAT DEMAND SKILL FROM THE MOST SEASONED OF PLAYERS. 18
-                  BEAUTIFULLY LANDSCAPED HOLES CREATE A SETTING UNLIKE ANY
-                  COURSE YOU'VE EVER PLAYED.
-                </h6>
+              <div className={styles.infoContainer}>
+                <h3 className={styles.gardensHeader}>THE GARDENS COURSE</h3>
+                <div className={styles.courseInfoContainer}>
+                  {/* <img src={hippo} /> */}
+                  <h5 className={styles.gardensInfo}>
+                    TURN MINIATURE GOLFING INTO A SYMPHONY OF FUN FOR THE WHOLE
+                    FAMILY. THE GARDENS COURSE IS 18 HOLES OF WHIMSICAL
+                    CHALLENGES THAT TAKE PLAYERS THROUGH FIVE ORCHESTRAL MOMENTS
+                    OF THE DISNEY CLASSIC, FANTASIA. DANCING FOUNTAINS, MUSICAL
+                    INSTRUMENTS AND ANIMATED SATUES ADD TO A GOLFING FANTASY
+                    YOU'LL ALWAYS REMEMBER.
+                  </h5>
+                </div>
+                <div className={styles.golfBallContainer}>
+                  <img src={golfBall} className={styles.golfBallPic} />
+                  <img src={golfBall} className={styles.golfBallPicCenter} />
+                  <img src={golfBall} className={styles.golfBallPic} />
+                </div>
+                <h3 className={styles.fairwaysHeader}>THE FAIRWAYS COURSE</h3>
+                <div className={styles.courseInfoContainer}>
+                  {/* <img /> */}
+                  <h5 className={styles.fairwaysInfo}>
+                    FOR A UNIQUEGOLFING EXPERIENCE, THIS COURSE BRINGS EXCITING
+                    CHALLENGE. ENJOY PURE GOLF ON A SMALL SCALE, COMPLETE WITH
+                    WATER HAZARDS, SAND TRAPS, GRASSY ROUGHS AND DOG-LEG BENDS
+                    THAT DEMAND SKILL FROM THE MOST SEASONED OF PLAYERS. 18
+                    BEAUTIFULLY LANDSCAPED HOLES CREATE A SETTING UNLIKE ANY
+                    COURSE YOU'VE EVER PLAYED.
+                  </h5>
+                </div>
               </div>
             </div>
           </div>
