b8beeece130401ebc8cfa636e2341438d23d92da
Emerson Downing <emersondowning@gmail.com>
2021-04-14 20:43:35 -0400

    got the information into the courses page

--- client/pages/courses.js
+++ client/pages/courses.js
@@ -22,11 +22,42 @@ export default function Courses() {
           <div className={styles.lightGreen}></div>
           <div className={styles.darkGreen}></div>
           <div className={styles.center}>
-            <div className={styles.center2}></div>
+            <div className={styles.center2}>
+              <h3 className={styles.gardensHeader}>THE GARDENS COURSE</h3>
+              <div className={styles.courseInfoContainer}>
+                <img />
+                <h6 className={styles.gardensInfo}>
+                  TURN MINIATURE GOLFING INTO A SYMPHONY OF FUN FOR THE WHOLE
+                  FAMILY. THE GARDENS COURSE IS 18 HOLES OF WHIMSICAL CHALLENGES
+                  THAT TAKE PLAYERS THROUGH FIVE ORCHESTRAL MOMENTS OF THE
+                  DISNEY CLASSIC, FANTASIA. DANCING FOUNTAINS, MUSICAL
+                  INSTRUMENTS AND ANIMATED SATUES ADD TO A GOLFING FANTASY
+                  YOU'LL ALWAYS REMEMBER.
+                </h6>
+              </div>
+              <h3 className={styles.fairwaysHeader}>THE FAIRWAYS COURSE</h3>
+              <div className={styles.courseInfoContainer}>
+                <img />
+                <h6 className={styles.fairwaysInfo}>
+                  FOR A UNIQUEGOLFING EXPERIENCE, THIS COURSE BRINGS EXCITING
+                  CHALLENGE. ENJOY PURE GOLF ON A SMALL SCALE, COMPLETE WITH
+                  WATER HAZARDS, SAND TRAPS, GRASSY ROUGHS AND DOG-LEG BENDS
+                  THAT DEMAND SKILL FROM THE MOST SEASONED OF PLAYERS. 18
+                  BEAUTIFULLY LANDSCAPED HOLES CREATE A SETTING UNLIKE ANY
+                  COURSE YOU'VE EVER PLAYED.
+                </h6>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <Button routePrevious="/" routeNext="/rules" text="Course Descriptions" />
+      <div className={styles.buttonContainer}>
+        <Button
+          routePrevious="/"
+          routeNext="/rules"
+          text="Course Descriptions"
+        />
+      </div>
     </>
   );
 }
