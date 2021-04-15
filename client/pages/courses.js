714f96a9760d7698de13ebab602695d538cd2c25
Emerson Downing <emersondowning@gmail.com>
2021-04-14 20:06:44 -0400

    got the favicon in there after far too long

--- client/pages/courses.js
+++ client/pages/courses.js
@@ -3,7 +3,29 @@ import Button from "../Components/button";
 export default function Courses() {
   return (
     <>
-      <h1>hello world</h1>
+      <div className={styles.mainContainer}>
+        <div className={styles.firstContainer}>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.center}>
+            <div className={styles.center2}></div>
+          </div>
+        </div>
+      </div>
       <Button routePrevious="/" routeNext="/rules" text="Course Descriptions" />
     </>
   );
