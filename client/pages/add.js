0a3fcbeaae0d69497366b8692544450fbab045cb
Emerson Downing <emersondowning@gmail.com>
2021-04-15 20:01:21 -0400

    got the functions to set which course the users are playing

--- client/pages/add.js
+++ client/pages/add.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Add.module.css";
 import ButtonNav from "../Components/button";
 import Button from "@material-ui/core/Button";
@@ -6,17 +6,8 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 
 export default function Add() {
-  const courseOptions = [
-    { key: "The Fairways", text: "The Fairways", value: "The Fairways" },
-    { key: "The Gardens", text: "The Gardens", value: "The Gardens" },
-  ];
-  const playerOptions = [
-    { key: "One", text: "One", value: "1" },
-    { key: "Two", text: "Two", value: "2" },
-    { key: "Three", text: "Three", value: "3" },
-    { key: "Four", text: "Four", value: "4" },
-    { key: "Five", text: "Five", value: "5" },
-  ];
+  const [fairways, setFairways] = useState(false);
+  const [gardens, setGardens] = useState(false);
 
   const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -24,9 +15,20 @@ export default function Add() {
     setAnchorEl(event.currentTarget);
   };
 
+  const closeFairways = () => {
+    setFairways(true);
+    handleClose();
+  };
+
+  const closeGardens = () => {
+    setGardens(true);
+    handleClose();
+  };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
     <>
       <div className={styles.mainContainer}>
@@ -67,8 +69,8 @@ export default function Add() {
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
-                <MenuItem onClick={handleClose}>The Fairways</MenuItem>
-                <MenuItem onClick={handleClose}>The Gardens</MenuItem>
+                <MenuItem onClick={closeFairways}>The Fairways</MenuItem>
+                <MenuItem onClick={closeGardens}>The Gardens</MenuItem>
               </Menu>
               <h3 className={styles.playerCountHeader}>HOW MANY PLAYERS</h3>
             </div>
