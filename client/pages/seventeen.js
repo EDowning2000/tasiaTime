048bf1c6887c37b2bcc36a8c3d5585b856ce2df6
Emerson Downing <emersondowning@gmail.com>
2021-04-26 22:23:35 -0400

    hole 17

---
+++ client/pages/seventeen.js
@@ -0,0 +1,85 @@
+import React, { useState, useContext } from "react";
+import styles from "../styles/Hole.module.css";
+import { GlobalState } from "../Components/GlobalState";
+import Input from "../Components/input";
+import Button from "../Components/button";
+
+export default function Seventeen() {
+  const {
+    selectedCourse,
+    playerOne,
+    playerTwo,
+    playerThree,
+    playerFour,
+    playerFive,
+    playerOneScores,
+    playerTwoScores,
+    playerThreeScores,
+    playerFourScores,
+    playerFiveScores,
+    playerOneTotal,
+    playerTwoTotal,
+    playerThreeTotal,
+    playerFourTotal,
+    playerFiveTotal,
+  } = useContext(GlobalState);
+
+  let holeNumber = 1;
+
+  const holeSelector = () => {
+    if (holeNumber < 18) {
+      holeNumber++;
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.mainContainer}>
+        <div className={styles.firstContainer}>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.lightGreen}></div>
+          <div className={styles.darkGreen}></div>
+          <div className={styles.center}>
+            <div className={styles.center2}>
+              <h1>Hole Number {holeNumber}</h1>
+              {/* inputs go here */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        {holeNumber < 18 && (
+          <>
+            <HoleSelectorButton
+              text={"Hole " + holeNumber}
+              holeSelectorForward={"/" + selectedCourse}
+              holeSelectorBack="/add"
+            />
+          </>
+        )}
+        {holeNumber == 18 && (
+          <>
+            <Button
+              text={"Hole " + holeNumber}
+              routePrevious="/add"
+              routeNext={"/" + selectedCourse}
+            />
+          </>
+        )}
+      </div>
+    </>
+  );
+}
