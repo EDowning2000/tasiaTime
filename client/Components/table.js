561bff8b196eb7ae81eb14020c0f9ecceb1ad240
Emerson Downing <emersondowning@gmail.com>
2021-05-02 20:14:21 -0400

    typo in 'playerOneScores literally 18 times on the table component

--- client/Components/table.js
+++ client/Components/table.js
@@ -62,7 +62,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE1}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[0]}</td>
+          <td className={styles.dataThree}>{playerOneScores[0]}</td>
           <td className={styles.dataThree}>{playerTwoScores[0]}</td>
           <td className={styles.dataThree}>{playerThreeScores[0]}</td>
           <td className={styles.dataThree}>{playerFourScores[0]}</td>
@@ -73,7 +73,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE2}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[1]}</td>
+          <td className={styles.dataThree}>{playerOneScores[1]}</td>
           <td className={styles.dataThree}>{playerTwoScores[1]}</td>
           <td className={styles.dataThree}>{playerThreeScores[1]}</td>
           <td className={styles.dataThree}>{playerFourScores[1]}</td>
@@ -84,7 +84,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE3}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[2]}</td>
+          <td className={styles.dataThree}>{playerOneScores[2]}</td>
           <td className={styles.dataThree}>{playerTwoScores[2]}</td>
           <td className={styles.dataThree}>{playerThreeScores[2]}</td>
           <td className={styles.dataThree}>{playerFourScores[2]}</td>
@@ -95,7 +95,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE4}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[3]}</td>
+          <td className={styles.dataThree}>{playerOneScores[3]}</td>
           <td className={styles.dataThree}>{playerTwoScores[3]}</td>
           <td className={styles.dataThree}>{playerThreeScores[3]}</td>
           <td className={styles.dataThree}>{playerFourScores[3]}</td>
@@ -106,7 +106,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE5}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[4]}</td>
+          <td className={styles.dataThree}>{playerOneScores[4]}</td>
           <td className={styles.dataThree}>{playerTwoScores[4]}</td>
           <td className={styles.dataThree}>{playerThreeScores[4]}</td>
           <td className={styles.dataThree}>{playerFourScores[4]}</td>
@@ -117,7 +117,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE6}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[5]}</td>
+          <td className={styles.dataThree}>{playerOneScores[5]}</td>
           <td className={styles.dataThree}>{playerTwoScores[5]}</td>
           <td className={styles.dataThree}>{playerThreeScores[5]}</td>
           <td className={styles.dataThree}>{playerFourScores[5]}</td>
@@ -128,7 +128,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE7}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[6]}</td>
+          <td className={styles.dataThree}>{playerOneScores[6]}</td>
           <td className={styles.dataThree}>{playerTwoScores[6]}</td>
           <td className={styles.dataThree}>{playerThreeScores[6]}</td>
           <td className={styles.dataThree}>{playerFourScores[6]}</td>
@@ -139,7 +139,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE8}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[7]}</td>
+          <td className={styles.dataThree}>{playerOneScores[7]}</td>
           <td className={styles.dataThree}>{playerTwoScores[7]}</td>
           <td className={styles.dataThree}>{playerThreeScores[7]}</td>
           <td className={styles.dataThree}>{playerFourScores[7]}</td>
@@ -150,7 +150,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE9}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[8]}</td>
+          <td className={styles.dataThree}>{playerOneScores[8]}</td>
           <td className={styles.dataThree}>{playerTwoScores[8]}</td>
           <td className={styles.dataThree}>{playerThreeScores[8]}</td>
           <td className={styles.dataThree}>{playerFourScores[8]}</td>
@@ -161,7 +161,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE10}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[9]}</td>
+          <td className={styles.dataThree}>{playerOneScores[9]}</td>
           <td className={styles.dataThree}>{playerTwoScores[9]}</td>
           <td className={styles.dataThree}>{playerThreeScores[9]}</td>
           <td className={styles.dataThree}>{playerFourScores[9]}</td>
@@ -172,7 +172,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE11}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[10]}</td>
+          <td className={styles.dataThree}>{playerOneScores[10]}</td>
           <td className={styles.dataThree}>{playerTwoScores[10]}</td>
           <td className={styles.dataThree}>{playerThreeScores[10]}</td>
           <td className={styles.dataThree}>{playerFourScores[10]}</td>
@@ -183,7 +183,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE12}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[11]}</td>
+          <td className={styles.dataThree}>{playerOneScores[11]}</td>
           <td className={styles.dataThree}>{playerTwoScores[11]}</td>
           <td className={styles.dataThree}>{playerThreeScores[11]}</td>
           <td className={styles.dataThree}>{playerFourScores[11]}</td>
@@ -194,7 +194,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE13}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[12]}</td>
+          <td className={styles.dataThree}>{playerOneScores[12]}</td>
           <td className={styles.dataThree}>{playerTwoScores[12]}</td>
           <td className={styles.dataThree}>{playerThreeScores[12]}</td>
           <td className={styles.dataThree}>{playerFourScores[12]}</td>
@@ -205,7 +205,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE14}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[13]}</td>
+          <td className={styles.dataThree}>{playerOneScores[13]}</td>
           <td className={styles.dataThree}>{playerTwoScores[13]}</td>
           <td className={styles.dataThree}>{playerThreeScores[13]}</td>
           <td className={styles.dataThree}>{playerFourScores[13]}</td>
@@ -216,7 +216,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE15}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[14]}</td>
+          <td className={styles.dataThree}>{playerOneScores[14]}</td>
           <td className={styles.dataThree}>{playerTwoScores[14]}</td>
           <td className={styles.dataThree}>{playerThreeScores[14]}</td>
           <td className={styles.dataThree}>{playerFourScores[14]}</td>
@@ -227,7 +227,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE16}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[15]}</td>
+          <td className={styles.dataThree}>{playerOneScores[15]}</td>
           <td className={styles.dataThree}>{playerTwoScores[15]}</td>
           <td className={styles.dataThree}>{playerThreeScores[15]}</td>
           <td className={styles.dataThree}>{playerFourScores[15]}</td>
@@ -238,7 +238,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE17}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[16]}</td>
+          <td className={styles.dataThree}>{playerOneScores[16]}</td>
           <td className={styles.dataThree}>{playerTwoScores[16]}</td>
           <td className={styles.dataThree}>{playerThreeScores[16]}</td>
           <td className={styles.dataThree}>{playerFourScores[16]}</td>
@@ -249,7 +249,7 @@ export default function Table({
           <th className={styles.holePar}>
             <div className={styles.circle}>{HOLE18}</div>
           </th>
-          <td className={styles.dataThree}>{playerOnescores[17]}</td>
+          <td className={styles.dataThree}>{playerOneScores[17]}</td>
           <td className={styles.dataThree}>{playerTwoScores[17]}</td>
           <td className={styles.dataThree}>{playerThreeScores[17]}</td>
           <td className={styles.dataThree}>{playerFourScores[17]}</td>
