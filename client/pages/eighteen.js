9892cdd90234a9877b0827c80c506e5390a44348
Emerson Downing <emersondowning@gmail.com>
2021-05-19 21:07:26 -0400

    hole 18 array methods finished

--- client/pages/eighteen.js
+++ client/pages/eighteen.js
@@ -6,35 +6,54 @@ import Input from "../Components/input";
 
 export default function Eighteen() {
   const {
-    selectedCourse,
     playerCount,
     playerOne,
     playerTwo,
     playerThree,
     playerFour,
     playerFive,
+    setPlayerOneScores,
     playerOneScores,
+    setPlayerTwoScores,
     playerTwoScores,
+    setPlayerThreeScores,
     playerThreeScores,
+    setPlayerFourScores,
     playerFourScores,
+    setPlayerFiveScores,
     playerFiveScores,
   } = useContext(GlobalState);
 
   const updateOneScore = e => {
-    playerOneScores[17] = e.target.value;
-    console.log(playerOneScores)
+    // playerOneScores[0] = e.target.value;
+    // setPlayerOneScores(playerOneScores.push(e.target.value));
+    setPlayerOneScores(playerOneScores => [...playerOneScores, e.target.value]);
+    console.log(playerOneScores);
   };
   const updateTwoScore = e => {
-    playerTwoScores[17] = e.target.value;
+    setPlayerTwoScores(playerTwoScores => [...playerTwoScores, e.target.value]);
+    console.log(playerTwoScores);
   };
   const updateThreeScore = e => {
-    playerThreeScores[17] = e.target.value;
+    setPlayerThreeScores(playerThreeScores => [
+      ...playerThreeScores,
+      e.target.value,
+    ]);
+    console.log(playerThreeScores);
   };
   const updateFourScore = e => {
-    playerFourScores[17] = e.target.value;
+    setPlayerFourScores(playerFourScores => [
+      ...playerFourScores,
+      e.target.value,
+    ]);
+    console.log(playerFourScores);
   };
   const updateFiveScore = e => {
-    playerFiveScores[17] = e.target.value;
+    setPlayerFiveScores(playerFiveScores => [
+      ...playerFiveScores,
+      e.target.value,
+    ]);
+    console.log(playerFiveScores);
   };
 
   return (
