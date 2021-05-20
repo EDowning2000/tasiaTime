0087d297dfd3d59bbb6c46ae61b6d9124ede20da
Emerson Downing <emersondowning@gmail.com>
2021-05-19 20:54:02 -0400

    hole 5 array methods finished

--- client/pages/five.js
+++ client/pages/five.js
@@ -12,28 +12,49 @@ export default function Five() {
     playerThree,
     playerFour,
     playerFive,
+    setPlayerOneScores,
     playerOneScores,
+    setPlayerTwoScores,
     playerTwoScores,
+    setPlayerThreeScores,
     playerThreeScores,
+    setPlayerFourScores,
     playerFourScores,
+    setPlayerFiveScores,
+    playerFiveScores,
     playerFiveScores,
   } = useContext(GlobalState);
 
   const updateOneScore = e => {
-    playerOneScores[4] = e.target.value;
+    // playerOneScores[0] = e.target.value;
+    // setPlayerOneScores(playerOneScores.push(e.target.value));
+    setPlayerOneScores(playerOneScores => [...playerOneScores, e.target.value]);
     console.log(playerOneScores);
   };
   const updateTwoScore = e => {
-    playerTwoScores[4] = e.target.value;
+    setPlayerTwoScores(playerTwoScores => [...playerTwoScores, e.target.value]);
+    console.log(playerTwoScores);
   };
   const updateThreeScore = e => {
-    playerThreeScores[4] = e.target.value;
+    setPlayerThreeScores(playerThreeScores => [
+      ...playerThreeScores,
+      e.target.value,
+    ]);
+    console.log(playerThreeScores);
   };
   const updateFourScore = e => {
-    playerFourScores[4] = e.target.value;
+    setPlayerFourScores(playerFourScores => [
+      ...playerFourScores,
+      e.target.value,
+    ]);
+    console.log(playerFourScores);
   };
   const updateFiveScore = e => {
-    playerFiveScores[4] = e.target.value;
+    setPlayerFiveScores(playerFiveScores => [
+      ...playerFiveScores,
+      e.target.value,
+    ]);
+    console.log(playerFiveScores);
   };
 
   return (
